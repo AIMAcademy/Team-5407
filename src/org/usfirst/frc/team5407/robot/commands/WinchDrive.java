3a1d13edc1eb8e666163f3c7726b07f24d23e296
Rick Castorani <rcastorani@aimpa.org>
2015-02-13 19:12:34 -0500

    Working on limit switches for Winch. Limit switch won't allow joystick to run winch if not already moving but won't stop it if it is moving.

--- src/org/usfirst/frc/team5407/robot/commands/WinchDrive.java
+++ src/org/usfirst/frc/team5407/robot/commands/WinchDrive.java
@@ -2,8 +2,10 @@ package org.usfirst.frc.team5407.robot.commands;
 
 import org.usfirst.frc.team5407.robot.OI;
 import org.usfirst.frc.team5407.robot.Robot;
+import org.usfirst.frc.team5407.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -17,11 +19,23 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  *
  */
 public class WinchDrive extends Command {
+	
+    DigitalInput winchLimitTop = RobotMap.winchLimitTop;
+    DigitalInput winchLimitBottom = RobotMap.winchLimitBottom;
+//    RobotMap.winchLimitTop.get();
 
     public WinchDrive() {
         // Use requires() here to declare subsystem dependencies
 		requires(Robot.winch);
     }
+    
+//	public boolean isTopSwitchOpen() {
+//		return winchLimitTop.get();
+//	}
+//
+//	public boolean isTopSwitchSet() {
+//		return limitSwitchup.get();
+//	}
 
     // Called just before this Command runs the first time
     protected void initialize() {
@@ -29,14 +43,14 @@ public class WinchDrive extends Command {
     }
 	
 	public void execute() {
-		if (Math.abs(OI.getStickY()) > 0.1) {//Adds a deadband to the joystick input
-	        // Robot.winch.disable();
-		    Robot.winch.driveWinch(OI.getStickY());
-		    // Robot.winch.onTarget();
-		}else {
-	        // Robot.winch.disable();
-			Robot.winch.driveWinch(0.0);			
-		}
+			if (Math.abs(OI.getStickY()) > 0.1) {//Adds a deadband to the joystick input
+		        // Robot.winch.disable();
+			    Robot.winch.driveWinch(OI.getStickY());
+			    // Robot.winch.onTarget();
+			}else {
+		        // Robot.winch.disable();
+				Robot.winch.driveWinch(0.0);			
+			}
 	}
 
     // Make this return true when this Command no longer needs to run execute()
