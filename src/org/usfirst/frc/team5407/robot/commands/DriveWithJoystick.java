f527bd78be0b53442a4980d9b9398dd7bf78714c
Aidan Rovinsky <arovinsky@aimpa.org>
2015-02-24 10:47:05 -0500

    fixed function name in drive withjoystick

--- src/org/usfirst/frc/team5407/robot/commands/DriveWithJoystick.java
+++ src/org/usfirst/frc/team5407/robot/commands/DriveWithJoystick.java
@@ -27,7 +27,7 @@ public class DriveWithJoystick extends Command {
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		Robot.chassis.driveWithJoystick(OI.getJoystick());
+		Robot.chassis.driveWithJoystick(OI.getJoystick0());
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
