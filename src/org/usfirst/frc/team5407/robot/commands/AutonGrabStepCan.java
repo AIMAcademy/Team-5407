6b5f9f349134ef4a1c5d283c5787ba19ec549b38
Aidan Rovinsky <arovinsky@aimpa.org>
2015-03-14 13:03:39 -0400

    changed variables for auton - should be working now and ready to field test

--- src/org/usfirst/frc/team5407/robot/commands/AutonGrabStepCan.java
+++ src/org/usfirst/frc/team5407/robot/commands/AutonGrabStepCan.java
@@ -9,11 +9,11 @@ public class AutonGrabStepCan extends CommandGroup {
     
     public  AutonGrabStepCan() {
     	addSequential(new ClawBackOpenClose(true));
-    	addSequential(new DriveRobot(0.5, 1, 0));
+    	addSequential(new DriveRobot(0.25, 1, 0));
     	addSequential(new ClawBackAuton(1.35, -1));
     	addSequential(new ClawBackOpenClose(false));
-    	addSequential(new ClawBackAuton(0.50, 1));
-    	addSequential(new DriveRobot(2, .75, 0));
+    	addSequential(new ClawBackAuton(1.25, 1));
+    	addSequential(new DriveRobot(3, -.75, 0));
 		addSequential(new DriveRobot(1.5, 0, 0.55));  // 90 degree turn
 
         // Add Commands here:
--- src/org/usfirst/frc/team5407/robot/commands/ClawBackAuton.java
+++ src/org/usfirst/frc/team5407/robot/commands/ClawBackAuton.java
@@ -36,6 +36,7 @@ public class ClawBackAuton extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+		Robot.claw_back.driveClaw(0);
     }
 
     // Called when another command which requires one or more of the same
