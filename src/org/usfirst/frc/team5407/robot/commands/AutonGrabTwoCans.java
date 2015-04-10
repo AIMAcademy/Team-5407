c645950907e34ac484a332dc0076f9396ed4f2d0
Aidan Rovinsky <arovinsky@aimpa.org>
2015-04-10 15:46:22 -0400

    two can auton almost working perfectly. added an extra extend claw to try to settle the first can. needs testing.

--- src/org/usfirst/frc/team5407/robot/commands/AutonGrabTwoCans.java
+++ src/org/usfirst/frc/team5407/robot/commands/AutonGrabTwoCans.java
@@ -12,21 +12,24 @@ public class AutonGrabTwoCans extends CommandGroup {
     	// Grab first can
     	addSequential(new ClawBackOpenClose(true));		// open claw
     	addSequential(new DriveRobot(0.2, 0, 0));		// pause for .2 seconds
-//    	addSequential(new DriveRobot(0.1, 1, 0));		// drive backwards to totes
-    	addSequential(new ClawBackAuton(1.8, -1));		// extend claw back
+    	addSequential(new DriveRobot(0.05, 1, 0));		// drive backwards to totes
+    	addSequential(new DriveRobot(0.2, 0, 0));		// pause for .2 seconds
+    	addSequential(new ClawBackAuton(1.75, -1));		// extend claw back
     	addSequential(new ClawBackOpenClose(false));	// close claw to grab can
     	addSequential(new DriveRobot(0.25, 0, 0));		// pause for .25 seconds
     	addSequential(new ClawBackAuton(1.5, 1));		// retract claw back
     	addSequential(new DriveRobot(0.25, 0, 0));		// pause for .25 seconds
+    	addSequential(new ClawBackAuton(0.25, -1));		// extend claw back
+    	addSequential(new DriveRobot(0.25, 0, 0));		// pause for .25 seconds
     	addSequential(new ClawBackOpenClose(true));		// open claw to drop can
     	addSequential(new DriveRobot(0.25, 0, 0));		// pause for .25 seconds
-    	addSequential(new ClawBackAuton(0.5, 1));		// retract claw back
-    	addSequential(new DriveRobot(0.5, -0.5, 0));	// drive forwards away from totes
+    	addSequential(new ClawBackAuton(1.25, 1));		// retract claw back
+    	addSequential(new DriveRobot(0.65, -0.5, 0));	// drive forwards away from totes
 
     	// Turn, drive, and grab second can
-    	addSequential(new DriveRobot(1.5, 0, -0.57));	// 90 degree turn left
-    	addSequential(new DriveRobot(1.50, 1, 0));		// drive backward to other can (approximate timing)
     	addSequential(new DriveRobot(1.5, 0, 0.57));	// 90 degree turn right
+    	addSequential(new DriveRobot(1.50, 1, 0));		// drive backward to other can (approximate timing)
+    	addSequential(new DriveRobot(1.5, 0, -0.57));	// 90 degree turn left
 //    	addSequential(new UltraSonic());						// turn left until find can
     	addSequential(new DriveRobot(0.25, 1, 0));		// drive backwards to totes
     	addSequential(new DriveRobot(0.2, 0, 0));		// pause for .2 seconds
@@ -37,7 +40,9 @@ public class AutonGrabTwoCans extends CommandGroup {
     	
     	// Move into scoring position
     	addSequential(new DriveRobot(2, -.75, 0));		// drive forwards to scoring position
-		addSequential(new DriveRobot(1.5, 0, 0.55));	// 90 degree turn right
+		addSequential(new DriveRobot(1.5, 0, -0.57));	// 90 degree turn left
+		addSequential(new DriveRobot(0.50, 0.5, 0));	// drive backwards away from wall
+
 
         // Add Commands here:
         // e.g. addSequential(new Command1());
